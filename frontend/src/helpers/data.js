6114505a40c8a7d6c32b27e9bc494edc00b2530b
Ida Tien Tran Manh <itmanh@ifi.uio.no>
2022-09-05 22:09:38 +0200

    2

---
+++ frontend/src/helpers/data.js
@@ -0,0 +1,23 @@
+export const colors = {
+  fire: "#F96D6B",
+  grass: "#49D0B0",
+  water: "#76BCFF",
+  electric: "#FCD973",
+  bug: "teal",
+  normal: "gray",
+
+};
+
+export const pokemons = [
+  { name: "Charizard", type: "Fire" },
+  { name: "Pikachu", type: "Electric" },
+  { name: "Squirtle", type: "Water" },
+  { name: "Bulbasaur", type: "Grass" },
+  { name: "Bulbasaur", type: "Grass" },
+];
+
+export const charizard = {
+  type: "Fire",
+  id: "#004",
+
+};
